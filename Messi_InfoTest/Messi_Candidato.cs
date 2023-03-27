772d0d2b1674b941a2a9d4367549bf7f0aa5f6fc
messi-joele <messi.nicolajoele.studente@itispaleocapa.it>
2023-03-27 08:46:05 +0200

    task1

---
+++ Messi_InfoTest/Messi_Candidato.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messi_InfoTest
+{
+    internal abstract    class Messi_Candidato
+    {
+        private int messi_matricola;
+        private string messi_nome;
+        
+        public Messi_Candidato(string Messi_nome, int Messi_matricola)
+        {
+            
+        }
+       
+        
+        private int Matricola
+        {
+            get { return messi_matricola; }
+            set { messi_matricola = value; }
+        }
+
+        private string Nome
+        {
+            get { return messi_nome; }
+            set { messi_nome = value; }
+        }
+        public Messi_Candidato()
+        {
+            messi_matricola = 0;
+            messi_nome = null;
+        }
+
+        public Messi_Candidato(int n)
+        {
+            messi_matricola = n;
+            messi_nome = null;
+        }
+        public Messi_Candidato(string s)
+        {
+            messi_matricola = 0;
+            messi_nome = s;
+        }
+
+        public Messi_Candidato(int n, string s)
+        {
+            messi_matricola = n;
+            messi_nome = s;
+        }
+
+
+        public abstract bool isIdoneo();
+        
+            
+
+
+        public abstract int punteggio();
+        
+    }
+
+
+}
