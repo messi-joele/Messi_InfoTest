d3f4cbf69f83c3849f840c2fda883d0c24e6b59c
messi-joele <messi.nicolajoele.studente@itispaleocapa.it>
2023-03-27 10:59:09 +0200

    Task 5 new

--- Messi_InfoTest/Form1.cs
+++ Messi_InfoTest/Form1.cs
@@ -16,14 +16,57 @@ namespace Messi_InfoTest
         {
             lista.Add(a);
         }
-        
 
-        public void Visualizza(Messi_Candidato a)
+
+        public string[] Visualizza(List<Messi_Candidato> a)
         {
-            lista.
+            string[] s = new string[999];
+            int n = 0;
+            foreach (Messi_Candidato c in lista)
+            {
+                s[n] = c.ToString();
+                n++;        
+            }
+
+            return s;
         }
 
+        public void Modifica(string nome, int nmatricola)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == nmatricola)
+                {
+                    lista[i].Nome = nome;
+                }
+            }
+        }
 
+        public void Elimina(int nmatricola)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == nmatricola)
+                {
+                    lista[i] = null;
+                }
+            }
+        }
+        public string[] Idonei()
+        {
+            string[] z = new string[999];
+            int n = 0;
+            foreach (Messi_Candidato a in lista)
+            {
+                bool b = a.isIdoneo();
+                if (b == true)
+                {
+                    z[n] = a.ToString();
+                    n++;
+                }
+            }
+            return z;
+        }
 
 
 
