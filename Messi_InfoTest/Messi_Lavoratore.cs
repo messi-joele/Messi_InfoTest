6f6d86d8a0a0431f2f1e47e0c32f179d86d918be
messi-joele <messi.nicolajoele.studente@itispaleocapa.it>
2023-03-27 09:33:22 +0200

    Task 3

---
+++ Messi_InfoTest/Messi_Lavoratore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messi_InfoTest
+{
+    public abstract class Messi_Lavoratore : Messi_Candidato
+    {
+        public int messi_esperienze;
+
+        public Messi_Lavoratore(int esperienze, int matricola, string nome) : base(matricola, nome)
+        {
+            messi_esperienze = esperienze;
+
+        }
+        public Messi_Lavoratore()
+        {
+            messi_esperienze = 0;
+        }
+        
+        public int Esperienze
+        {
+            get
+            {
+                return messi_esperienze;
+            }
+            set
+            {
+                if (value >= 0 && value <= 5)
+                    messi_esperienze = value;
+                else
+                    throw new Exception("Numero di esperienze non accettato");
+            }
+        }
+
+
+
+    }
+}
